9ce080809f698ea5fe74044b719abc8ed7342e97
nonsens <nonsens1212@gmail.com>
2021-05-23 20:30:59 +0400

    Добавил реализацию создания отчета в модели ContractPIR, ContractSMR и Powerline

---
+++ PowerlineTrackerNew.Services/Reports/PowerlineReportService.cs
@@ -0,0 +1,29 @@
+﻿using OfficeOpenXml;
+using PowerlineTrackerNew.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerlineTrackerNew.Services.Reports
+{
+    public class PowerlineReportService : IExcelDownloadable
+    {
+        public void BuildExcelWorksheet(ExcelWorksheet worksheet)
+        {
+            List<Powerline> Powerlines = new List<Powerline>
+            {
+                new Powerline { Name = "test powerline 1", Comments = "test comments 1", ID = 1, ConractSMR = new ContractSMR { Number = 1111 }, ContractPIR = new ContractPIR { Number = 2222 }, InternalNotes = new List<InternalNote> { new InternalNote { Theme = "test internal Note 1" } } },
+                new Powerline { Name = "test powerline 2", Comments = "test comments 2", ID = 2, ConractSMR = new ContractSMR { Number = 3333 }, ContractPIR = new ContractPIR { Number = 4444 }, InternalNotes = new List<InternalNote> { new InternalNote { Theme = "test internal Note 2" } } }
+            };
+
+            for (int i = 0; i < Powerlines.Count; i++)
+            {
+                worksheet.Cells[i + 2, 1].Value = $"ID: {Powerlines[i].ID} ";
+                worksheet.Cells[i + 2, 2].Value = $"Наименование объекта: {Powerlines[i].Name}";
+                worksheet.Cells[i + 2, 3].Value = $"Номер договора ПИР: {Powerlines[i].ContractPIR}";
+                worksheet.Cells[i + 2, 3].Value = $"Номер договора СМР: {Powerlines[i].ConractSMR}";
+                worksheet.Cells[i + 2, 3].Value = $"Служебные записки: {Powerlines[i].InternalNotes[0].Theme}";
+            }
+        }
+    }
+}
---
+++ PowerlineTrackerNew/Controllers/ContractPIRController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PowerlineTrackerNew.Services;
+using PowerlineTrackerNew.Services.Reports;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerlineTrackerNew.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContractPIRController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TypeOfContractController> _logger;
+
+        public ContractPIRController(ILogger<ContractPIRController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public IActionResult DownloadReport()
+        {
+            ContractPIRReportService service = new ContractPIRReportService();
+            ExcelBuilder builder = new ExcelBuilder();
+            byte[] file =  builder.BuildFile(service);
+            MemoryStream stream = new MemoryStream(file);
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");
+        }
+    }
+}
